338ed4a6b3d1fbf53f836412d3b42b2ac42ffca8
Polichinel <simpo@sol.dk>
2021-03-11 06:31:33 +0100

    c_est = 16, c_pred = 4

--- temporal_two_trends_ExpQuad.py
+++ temporal_two_trends_ExpQuad.py
@@ -35,7 +35,7 @@ two_trend_ExpQuad_dict = {}
 C_pred = 4 # 100
 
 # minimum number of conf in timeslines used to est hyper parameters
-C_est = 100#16
+C_est = 16 #100
 
 # conflict type. Som might need lower c_est than 100 to work
 conf_type = 'ged_best_sb' #['ged_best_sb', 'ged_best_ns', 'ged_best_os', 'ged_best']
