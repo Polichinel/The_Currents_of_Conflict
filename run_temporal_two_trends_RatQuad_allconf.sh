17b228078b7ed5fe9fc029738a1fc98a1a5b6b0e
Polichinel <simpo@sol.dk>
2021-03-12 12:39:14 +0100

    Rational cov_s, Cc_est = 32, c_pred = 1

---
+++ run_temporal_two_trends_RatQuad_allconf.sh
@@ -0,0 +1,11 @@
+# Go to the directory from where the job was submitted (initial directory is $HOME)
+cd /home/projects/ku_00017/people/simpol/scripts/currents/the_currents_of_conflict
+
+# Load all required modules for the job
+module load tools
+module load anaconda3/2020.07
+
+# This is where the work is done
+# Make sure that this script is not bigger than 64kb ~ 150 lines, otherwise put in seperat script and execute from here
+python temporal_two_trends_RatQuad_allconf.py
+ 
