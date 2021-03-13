1239f2d0aa38e5a3b31c405ce6db92578d002d56
Polichinel <simpo@sol.dk>
2021-03-13 20:30:17 +0100

    fixed dum typos

--- temporal_two_trends_Matern52_sb.py
+++ temporal_two_trends_Matern52_sb.py
@@ -189,8 +189,8 @@ two_trend_Matern52_dict[pre_script_df_results] = df_results
 two_trend_Matern52_dict[pre_script_df] = df_merged
             
 
-#new_file_name = "/home/polichinel/Documents/Articles/conflict_prediction/data/ViEWS/two_trend_Matern52_allconf_dict.pkl"
-new_file_name = '/home/projects/ku_00017/data/generated/currents/two_trend_Matern52_allconf_dict.pkl'
+#new_file_name = "/home/polichinel/Documents/Articles/conflict_prediction/data/ViEWS/two_trend_Matern52_sb_dict.pkl"
+new_file_name = '/home/projects/ku_00017/data/generated/currents/two_trend_Matern52_sb_dict.pkl'
 output = open(new_file_name, 'wb')
 pickle.dump(two_trend_Matern52_dict, output)
 output.close()
