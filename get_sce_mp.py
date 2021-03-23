8b4e990b13160972f87239d63f8ec30550fb7f22
Polichinel <spm@ifs.ku.dk>
2021-03-24 00:31:02 +0100

    trying with spares..

--- get_sce_mp.py
+++ get_sce_mp.py
@@ -57,8 +57,11 @@ with pm.Model() as model:
     # mean func. (constant) 
     mean =  pm.gp.mean.Zero()
 
+    # sample and split X,y ---------------------------------------------  
+    sample_pr_id = sample_conflict_timeline(conf_type = conf_type, df = df, train_id = train_id, test_id = val_id, C = C_est)
+
     # GP
-    gp = pm.gp.Marginal(mean_func=mean ,cov_func=cov)
+    gp = pm.gp.MarginalSparse(mean_func=mean ,cov_func=cov)
  
     # always prudent:
     df_sorted = df.sort_values(['pg_id', 'month_id'])
@@ -67,18 +70,21 @@ with pm.Model() as model:
     coord_len = df.groupby(['xcoord', 'ycoord']).sum().shape[0]
     y = theano.shared(np.zeros(coord_len), 'y')
     X = theano.shared(np.zeros([coord_len, 2]), 'X')
-    
+
+    # this does not vary here:
+    Xu = theano.shared(df[(df['id'].isin(sample_pr_id))][['xcoord','ycoord']].values, 'Xu')
+
     # loop
-    month_ids = df[df['id'].isin(train_id)]['month_id'].unique()[:5] # note!!!
-    n = len(month_ids)
+    month_ids = df[df['id'].isin(train_id)]['month_id'].unique()[:5] # note
+    n = month_ids.shape[0]
 
     for i, j in enumerate(month_ids):
-        print(f'{i}/{n} (estimation)', end='\r')       
+        print(f'{i+1}/{n} (estimation)', end='\r')       
 
         y.set_value(np.log(df[(df['id'].isin(train_id)) & (df['month_id'] == j)]['ged_best_sb'].values + 1))
         X.set_value(df[(df['id'].isin(train_id)) & (df['month_id'] == j)][['xcoord','ycoord']].values)
 
-        y_ = gp.marginal_likelihood(f"y_{i}", X=X, y=y, noise= σ)
+        y_ = gp.marginal_likelihood(f"y_{i}", X=X, Xu = Xu, y=y, noise= σ)
 
     mp = pm.find_MAP()
 print('Got mp')
