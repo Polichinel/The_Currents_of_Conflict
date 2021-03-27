041e7a748d202e54af5ebd1238daca0182d2c37b
Polichinel <spm@ifs.ku.dk>
2021-03-27 03:57:50 +0100

    sample same as cm

--- utils_dce.py
+++ utils_dce.py
@@ -106,20 +106,22 @@ def test_val_train(df, info = True, test_time = False):
         return(train_id, test_id)
 
 
+def sample_conflict_timeline_old(conf_type, df, train_id, test_id, C=5):
 
-def sample_conflict_timeline(conf_type, df, train_id, test_id, C=12):
+    """ This function samples N time-lines contining c>=C conflicts. 
+    As default it will try to get the val_id. Error will come if it does not exits"""
 
-    """This function samples N time-lines contining C >= conflicts in at least one year.
-    Default C = 12, so that is one year with a conflict each day."""
+    # so now you just use the same as for cm.
+    conf_type = 'ged_best_sb'
+    dummy = 'ged_dummy_sb'
 
     # sort the df - just in case
     df_sorted = df.sort_values(['pg_id', 'month_id'])
 
-    df_sorted['new_dummy'] =  (df_sorted[conf_type] >= 1)*1 # temp! test! stuff!
-
     # groupby gids and get total events
-    df_sum = df_sorted.groupby(['pg_id', 'year']).sum()[['new_dummy']].reset_index()
-    sample_pr_id = df_sum[df_sum['new_dummy'] >= C]['pg_id'].unique()
+    df_sb_total_events = df.groupby(['pg_id']).sum()[dummy].reset_index().rename(columns = {dummy:'ged_total_events'})
+    
+    sample_pr_id = df_sb_total_events[df_sb_total_events['ged_total_events'] >= C]['pg_id'].unique()
 
     return(sample_pr_id)
 
