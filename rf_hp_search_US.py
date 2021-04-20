0c2b37df531602be61ff032c08c0ed7bc126d316
Polichinel <spm@ifs.ku.dk>
2021-04-20 10:07:05 +0200

    ..

--- rf_hp_search_US.py
+++ rf_hp_search_US.py
@@ -136,8 +136,8 @@ hp_df = pd.DataFrame({'n_estimators' : n_estimators_list, 'max_depth' : max_dept
 
 
 print('Pickling..')
-new_file_name = '/home/projects/ku_00017/data/generated/currents/rf_hp_df.pkl'
-#new_file_name = '/home/simon/Documents/Articles/conflict_prediction/data/computerome/currents/rf_hp_df.pkl'
+new_file_name = '/home/projects/ku_00017/data/generated/currents/rf_hp_US_df.pkl'
+#new_file_name = '/home/simon/Documents/Articles/conflict_prediction/data/computerome/currents/rf_hp_US_df.pkl'
 output = open(new_file_name, 'wb')
 pickle.dump(hp_df, output)
 output.close()
